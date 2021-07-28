4ebe6c3cec903b66bc991348dbbd3d5a97ab0b44
burahankrmz <burahankrmz@gmail.com>
2021-07-28 21:12:56 +0300

    Login Page Created And Login Functions added

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -7,20 +7,129 @@ import 'package:flutter/material.dart';
 import '../Store/storehome.dart';
 import 'package:e_shop/Config/config.dart';
 
-
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
 
+class _LoginState extends State<Login> {
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-
-
-
-class _LoginState extends State<Login>
-{
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              width: _screenWidth / 3.5,
+              height: _screenHeight / 3.5,
+              child: Image.asset(
+                'images/login.png',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Login to your account",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    controller: _emailTextEditingController,
+                    data: Icons.mail,
+                    hintText: 'Mail Adress',
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    controller: _passwordTextEditingController,
+                    data: Icons.lock,
+                    hintText: 'PassWord',
+                    isObsecure: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _emailTextEditingController.text.isNotEmpty &&
+                              _passwordTextEditingController.text.isNotEmpty
+                          ? loginUser()
+                          : showDialog(
+                              context: (context),
+                              builder: (c) {
+                                return ErrorAlertDialog(
+                                  message: 'Please write email and password.',
+                                );
+                              });
+                    },
+                    child: Text(
+                      'Login',
+                    ),
+                    style: ElevatedButton.styleFrom(primary: Colors.pink),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  Container(
+                    height: 4.0,
+                    width: _screenWidth * 0.8,
+                    color: Colors.pink,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "i'm Admin",
+                      style: TextStyle(
+                          color: Colors.pink, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  loginUser() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return LoadingAlertDialog(
+            message: "Authenticating, Please wait...",
+          );
+        });
+    FirebaseUser firebaseUser;
+    await _auth
+        .signInWithEmailAndPassword(
+      email: _emailTextEditingController.text.trim(),
+      password: _passwordTextEditingController.text.trim(),
+    )
+        .then((value) {
+      firebaseUser = value.user;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorAlertDialog(
+              message: error.message.toString(),
+            );
+          });
+    });
   }
 }
